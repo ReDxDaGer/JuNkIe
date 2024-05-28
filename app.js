812cfd3329216d6c5c7d8380c29a2ff845898536
ReDxDaGer <yashmehrotra45@gmail.com>
2024-05-29 03:24:47 +0530

    Updated the connection

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const db = require('./config/mongoose-connection');
+const ownersRoute = require('./routes/ownersRouter');
+const usersRoute = require('./routes/usersRouter');
+const productsRoute = require('./routes/productsRouter');
+const { error } = require('console');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use("/owner" , ownersRoute);
+app.use("/user" , usersRoute);
+app.use("/product" , productsRoute);
 
 app.listen(3000, () => {
-    console.log("localhost: https://localhost:3000/")
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log("localhost: https://localhost:3000/")
+    }
 });
